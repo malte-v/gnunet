2c797708b38729effe82328484d67a2be02412ed
t3sserakt <t3ss@posteo.de>
2020-09-02 16:22:21 +0200

    - added second make check, because there is still an issue with second communnicator process not finishing correctly.

--- .buildbot/firefly-x86_64-amdepyc_test.sh
+++ .buildbot/firefly-x86_64-amdepyc_test.sh
@@ -5,5 +5,7 @@
 pushd src/transport
 make check TESTS='test_communicator_basic-tcp'
 cat test-suite.log
-pkill --signal 9 -U buildbot gnunet 
+pkill --signal 9 -U buildbot gnunet
+make check TESTS='test_communicator_rekey-tcp'
+pkill --signal 9 -U buildbot gnunet
 popd
