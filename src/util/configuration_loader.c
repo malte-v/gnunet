092c2c714e085a66834127bfb1e841486fd4f54c
Alessio Vanni <vannilla@firemail.cc>
2020-10-26 01:26:27 +0100

    - fix '-c' in applications without base config

--- src/util/configuration_loader.c
+++ src/util/configuration_loader.c
@@ -63,14 +63,16 @@ GNUNET_CONFIGURATION_load (struct GNUNET_CONFIGURATION_Handle *cfg,
     GNUNET_free (ipath);
   }
 
-  if (GNUNET_SYSERR ==
-      GNUNET_CONFIGURATION_load_from (cfg,
-                                      baseconfig))
+  char *dname = GNUNET_STRINGS_filename_expand (baseconfig);
+  GNUNET_free (baseconfig);
+
+  if (GNUNET_YES == GNUNET_DISK_directory_test (dname, GNUNET_YES) &&
+      GNUNET_SYSERR == GNUNET_CONFIGURATION_load_from (cfg, dname))
   {
-    GNUNET_free (baseconfig);
+    GNUNET_free (dname);
     return GNUNET_SYSERR;       /* no configuration at all found */
   }
-  GNUNET_free (baseconfig);
+  GNUNET_free (dname);
   if ((NULL != filename) &&
       (GNUNET_OK != GNUNET_CONFIGURATION_parse (cfg, filename)))
   {
